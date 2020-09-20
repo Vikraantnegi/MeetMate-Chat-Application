b92d1368156a9226f5ec278ddfd9289bb7732418
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2020-09-20 19:31:43 +0530

    Adding user functionalities and states

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import qString from 'query-string';
+import io from 'socket.io-client';
 import './Chat.css';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({location}) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const endpoint =  'localhost:5000';
+    useEffect(()=>{
+        const {name, room} = qString.parse(location.search);
+        socket= io(endpoint);
+        setName(name);
+        setRoom(room);
+        
+        socket.emit('join', {name, room}, ({})=>{
+        });
+
+        return ()=>{
+            socket.emit('disconnect');
+            socket.off();
+        }
+    }, [endpoint, location.search]);
+
+
     return(
-        <h1>Chat</h1>
+        <div>
+
+        </div>
     )
 }
 
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const socketio = require('socket.io');
 const http = require('http');
 const router = require('./router');
 
+const {addUser, removeUser, getUser, getUsersRoom} = require('./users');
+
 const port = process.env.port || 5000;
 
 const app = express();
@@ -11,6 +13,24 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
     console.log('We have a new user!');
+    socket.on('join', ({name,room}, callback)=>{  
+        const {error, user} = addUser({id:socket.id, name, room});
+        if(error) return callback(error);
+
+        socket.emit('Message', {user:'admin', text:`${user.name}, Welcome to ${user.room}`});
+        socket.broadcast.to(user.room).emit('Message', {user:'admin', text: `${user.name} has joined the ${user.room}`});
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('SendMessage', (text, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('Message', {user:user.name, text: text});
+
+        callback();
+    });
+
     socket.on('disconnect', () => {
         console.log('Sorry, but the user just left!');
     });
